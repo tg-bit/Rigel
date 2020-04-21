bec64085f11d14ce49c0a280467b3d0411a05257
tg-bit <62854043+tg-bit@users.noreply.github.com>
2020-04-21 23:02:13 +0300

    Rigel-v01

--- instagram/BruteSploit-master/tools/instabrute.py
+++ instagram/BruteSploit-master/tools/instabrute.py
@@ -355,7 +355,7 @@ if __name__ == "__main__":
     print("""| \__  _/           | '_ \| |_) | | | | | | |  _|       |""")
     print("""|   ( (             | |_) |  _ <| |_| | | | | |___      |""")
     print("""|    ) )            |_.__/|_| \_\_____/ |_| |_____|     |""")
-    print("""|   (_(                                                 |""")
+    print("""|   (_(                                          rigel  |""")
     print("""'-------------------------------------------------------'""")
 
     print(bcolors.OKGREEN + "[+] Username Loaded:", bcolors.BOLD + USER + bcolors.ENDC)
